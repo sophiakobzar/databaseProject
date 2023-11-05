b5845c5c3043d8988d3139eaca93a896d486a10e
vrao0409 <71802067+vrao0409@users.noreply.github.com>
2023-11-05 17:13:37 -0600

    New items, orders, and customers added to database

--- grocerystore.sql
+++ grocerystore.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 05, 2023 at 05:21 PM
+-- Generation Time: Nov 05, 2023 at 11:50 PM
 -- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -41,13 +41,12 @@ CREATE TABLE `customer` (
 
 INSERT INTO `customer` (`Name`, `password`, `customerID`, `Payment Method`, `email`) VALUES
 ('Sophia Kobzar', 'password@123', 1234, '512-12433-12', 'smk@utd.edu'),
-('Sophia Kobzar', '', 4978, '9230392 / 233', 'smk180007@utdallas.edu'),
-('Emily Ta', '', 4548, '231223 211', 'eta2@gmail.com'),
-('Danny Turner', '', 9956, '12312 123', 'dt@gmail.com'),
-('test', '', 3493, '342421', 'tes'),
-('Bella Smith', '', 3024, '1341341 134', 'bella@gmail.com'),
-('test', '', 7191, '12312 122', 'asd@gmail.com'),
-('Zack Martin', '', 6084, '13412342 342', 'mt@outlook.com');
+('Emily Ta', 'ET1234', 4548, '2313-2345-9475-1934', 'eta2@gmail.com'),
+('Danny Turner', 'DT3452', 9956, '1231-2123-4856-9854', 'dt2004@gmail.com'),
+('Richard Ma', 'RM2839', 3493, '3424-2145-2938-9485', 'rm@outlook.com'),
+('Bella Smith', 'BS3498', 3024, '1341-3411-3456-8534', 'bella@gmail.com'),
+('Gia Long', 'GL2928', 7191, '1231-2122-9384-8293', 'gial@gmail.com'),
+('Zack Martin', 'ZM1234', 6084, '1341-2342-8970-9342', 'zm@outlook.com');
 
 -- --------------------------------------------------------
 
@@ -75,7 +74,21 @@ INSERT INTO `items` (`imageName`, `itemName`, `price`, `quantity`, `returnable`,
 ('carrot.jpg', 'Carrot', '$0.79/lb', 3, 1, 123, 8765),
 ('milk.jpg', 'Milk', '$0.99/lb', 5, 1, 567, 1209),
 ('eggs.jpg', 'Eggs', '$3.00/ea', 5, 1, 987, 2531),
-('orange.jpg', 'Orange', '$1.59/lb', 4, 1, 123, 9487);
+('orange.jpg', 'Orange', '$1.59/lb', 4, 1, 123, 9487),
+('chips.jpg', 'Chips', '$2.00/ea', 5, 1, 124, 2736),
+('dishsoap.jpg', 'Dishsoap', '$3.50/ea', 6, 1, 122, 5796),
+('protein powder.jpg', 'Protein Powder', '$12.00/ea', 10, 1, 112, 9375),
+('sriracha.jpg', 'Sriracha', '$5.00/ea', 5, 1, 111, 2938),
+('water.jpg', 'Water', '$1.50/ea', 5, 1, 384, 1029),
+('lettuce.jpg', 'Lettuce', '$1.00/lb', 5, 1, 684, 2956),
+('mayo.jpg', 'Mayonnaise', '$4.00/ea', 6, 1, 934, 284),
+('oj.jpg', 'Orange Juice', '$6.00/ea', 10, 1, 386, 1928),
+('vegetableoil.jpg', 'Vegetable Oil', '$4.50/ea', 5, 1, 439, 2948),
+('redonion.jpg', 'Red Onion', '$1.00/lb', 5, 1, 937, 4964),
+('bread.jpg', 'Bread', '$5.00/ea', 5, 1, 475, 9382),
+('icecream.jpg', 'Vanilla Ice Cream', '$4.00/ea', 10, 1, 839, 9549),
+('grape.jpg', 'Grapes', '$2.00/lb', 10, 1, 489, 7372),
+('acv.jpg', 'Apple Cider Vinegar', '$6.00/ea', 10, 1, 822, 3937);
 
 -- --------------------------------------------------------
 
@@ -97,15 +110,14 @@ CREATE TABLE `order` (
 --
 
 INSERT INTO `order` (`OrderID`, `subtotal`, `address`, `discount`, `totalPrice`, `CustomerID`) VALUES
-(54312, 1.98, '4005 Panther Way, Plano, TX 75074', 0.1, 1.88, 1234),
-(15399, 0, '', 0, 1.98, 0),
 (19825, 2.17, '1122 Parkway Plano Tx 75074', 0, 2.17, 4978),
 (64606, 0.99, '5633 Plano Parkway Garland Tx 75074', 0, 0.99, 4548),
 (80744, 0.99, '4322 Parker Plano Tx 75074', 0, 0.99, 9956),
-(62372, 0.59, 'sdrasd', 0, 0.59, 3493),
 (75217, 0.59, '1222 test rd Garland TX 75032', 0, 0.59, 3024),
-(75304, 1.58, 'mwsefd', 0, 1.58, 7191),
-(82413, 3.17, '1231 st', 0, 3.17, 6084);
+(11111, 3, '18614 Oxenberg Lane, Richardson, TX 75080', 0, 3, 1683),
+(22222, 4, '702 Arden Oaks, Dallas, TX 75074', 0, 4, 6497),
+(93972, 10, '394 Luxemburg Lane Austin, TX 74930', 3, 7, 9467),
+(49786, 17, '9300 Santa Fe Blvd, Plano, TX 75074', 0, 17, 1221);
 
 -- --------------------------------------------------------
 
