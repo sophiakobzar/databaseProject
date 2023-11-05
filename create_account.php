ed8910f3642c019e1d7eabce9c8bf4dcb0161c66
JonAbshiro <jonabshiro123@gmail.com>
2023-11-05 17:15:23 -0600

    -Fixed create account

--- create_account.php
+++ create_account.php
@@ -4,6 +4,25 @@ $username = "root";
 $password = "";
 $dbname = "grocerystore";
 
+function generateCustomerID($conn) {
+  $customerID = mt_rand(1000, 9999); // Generate a random order ID
+    $sql = "SELECT CustomerID FROM `customer` WHERE customerID = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $customerID);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $stmt->close();
+    
+    // Check if the generated order ID already exists
+    if ($result->num_rows > 0) {
+        // If it exists, generate a new one recursively
+        return generateUniqueOrderID($conn);
+    }
+    
+    return $customerID; // Return the unique order ID
+}
+
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -14,13 +33,14 @@ if ($conn->connect_error) {
 
 // Get data from form
 $name = $_POST['Name'];
-$paymentMethod = $_POST['Payment Method'];
+$paymentMethod = $_POST['PaymentMethod'];
 $email = $_POST['email'];
 $password = $_POST['password']; // Don't forget to hash this before inserting
+$customerID = generateCustomerID($conn);
 
 // Prepare and bind
-$stmt = $conn->prepare("INSERT INTO `customer` (`Name`, `Payment Method`, `email`, `password`, `customerID`) VALUES (?, ?, ?, ?, 23)");
-$stmt->bind_param("ssss", $name, $paymentMethod, $email, $password);
+$stmt = $conn->prepare("INSERT INTO `customer` (`Name`, `Payment Method`, `email`, `password`, `customerID`) VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("ssssi", $name, $paymentMethod, $email, $password, $customerID);
 
 $stmt->execute();
 
