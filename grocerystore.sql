0d003178c354863f6ff5a3fb41eb5167aa900ac4
sophiakobzar <31572998+sophiakobzar@users.noreply.github.com>
2023-11-05 19:00:57 -0600

    database

--- grocerystore.sql
+++ grocerystore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 06, 2023 at 01:18 AM
+-- Generation Time: Nov 06, 2023 at 02:00 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -40,12 +40,16 @@ CREATE TABLE `customer` (
 --
 
 INSERT INTO `customer` (`Name`, `password`, `customerID`, `Payment Method`, `email`) VALUES
-('Bella Smith', 'BS3498', 3024, '1341-3411-3456-8534', 'bella@gmail.com'),
+('Nicky Caine', '', 2180, '4656-4563-5436-6354', 'nickyC123@gmail.com'),
+('Timmy Turner', '', 3230, '1313-4321-3411-1233', 'timGamer@gmail.com'),
 ('Richard Ma', 'RM2839', 3493, '3424-2145-2938-9485', 'rm@outlook.com'),
+('Donald Duck', '', 4066, '1234-3432-4321-8775', 'duck@gmail.com'),
 ('Emily Ta', 'ET1234', 4548, '2313-2345-9475-1934', 'eta2@gmail.com'),
+('Sophia Kobzar', '', 5150, '4331-3143-1324-1345', 'smk@utd.edu'),
 ('Zack Martin', 'ZM1234', 6084, '1341-2342-8970-9342', 'zm@outlook.com'),
-('Donald Duck', '', 6121, '1234-3432-4321-8775', 'duck@gmail.com'),
+('Bella Smith', 'BS3498', 6121, '1341-3411-3456-8534', 'bella@gmail.com'),
 ('Gia Long', 'GL2928', 7191, '1231-2122-9384-8293', 'gial@gmail.com'),
+('Cody Martin', '', 8000, '5644-4545-4566-4352', 'codyM@gmail.com'),
 ('Danny Turner', 'DT3452', 9956, '1231-2123-4856-9854', 'dt2004@gmail.com');
 
 -- --------------------------------------------------------
@@ -69,25 +73,25 @@ CREATE TABLE `items` (
 --
 
 INSERT INTO `items` (`imageName`, `itemName`, `price`, `quantity`, `returnable`, `warehouseID`, `itemID`) VALUES
-('mayo.jpg', 'Mayonnaise', '$4.00/ea', 6, 1, 934, 284),
-('water.jpg', 'Water', '$1.50/ea', 4, 1, 384, 1029),
-('milk.jpg', 'Milk', '$0.99/lb', 5, 1, 567, 1209),
-('oj.jpg', 'Orange Juice', '$6.00/ea', 10, 1, 386, 1928),
+('mayo.jpg', 'Mayonnaise', '$4.00/ea', 5, 1, 934, 284),
+('water.jpg', 'Water', '$1.50/ea', 7, 1, 384, 1029),
+('milk.jpg', 'Milk', '$0.99/lb', 7, 1, 567, 1209),
+('oj.jpg', 'Orange Juice', '$6.00/ea', 9, 1, 386, 1928),
 ('eggs.jpg', 'Eggs', '$3.00/ea', 5, 1, 987, 2531),
-('chips.jpg', 'Chips', '$2.00/ea', 4, 1, 124, 2736),
-('sriracha.jpg', 'Sriracha', '$5.00/ea', 4, 1, 111, 2938),
-('vegetableoil.jpg', 'Vegetable Oil', '$4.50/ea', 5, 1, 439, 2948),
+('chips.jpg', 'Chips', '$2.00/ea', 7, 1, 124, 2736),
+('sriracha.jpg', 'Sriracha', '$5.00/ea', 9, 1, 111, 2938),
+('vegetableoil.jpg', 'Vegetable Oil', '$4.50/ea', 9, 1, 439, 2948),
 ('lettuce.jpg', 'Lettuce', '$1.00/lb', 5, 1, 684, 2956),
 ('acv.jpg', 'Apple Cider Vinegar', '$6.00/ea', 10, 1, 822, 3937),
-('banana.jpg', 'Banana', '$0.59/lb', 2, 1, 123, 4917),
+('banana.jpg', 'Banana', '$0.59/lb', 7, 1, 123, 4917),
 ('redonion.jpg', 'Red Onion', '$1.00/lb', 5, 1, 937, 4964),
 ('dishsoap.jpg', 'Dishsoap', '$3.50/ea', 6, 1, 122, 5796),
-('apple.jpg', 'Apple', '$0.99/lb', 4, 1, 567, 6382),
+('apple.jpg', 'Apple', '$0.99/lb', 6, 1, 567, 6382),
 ('grape.jpg', 'Grapes', '$2.00/lb', 10, 1, 489, 7372),
-('carrot.jpg', 'Carrot', '$0.79/lb', 3, 1, 123, 8765),
+('carrot.jpg', 'Carrot', '$0.79/lb', 8, 1, 123, 8765),
 ('protein powder.jpg', 'Protein Powder', '$12.00/ea', 10, 1, 112, 9375),
 ('bread.jpg', 'Bread', '$5.00/ea', 5, 1, 475, 9382),
-('orange.jpg', 'Orange', '$1.59/lb', 4, 1, 123, 9487),
+('orange.jpg', 'Orange', '$1.59/lb', 9, 1, 123, 9487),
 ('icecream.jpg', 'Vanilla Ice Cream', '$4.00/ea', 10, 1, 839, 9549);
 
 -- --------------------------------------------------------
@@ -110,9 +114,12 @@ CREATE TABLE `order` (
 --
 
 INSERT INTO `order` (`OrderID`, `subtotal`, `address`, `discount`, `totalPrice`, `CustomerID`) VALUES
+(18375, 6.49, '509 3rd Court Anchorage, AK 99504', NULL, 6.49, 5150),
+(20379, 10.5, '663 New Saddle Dr. New Baltimore, MI 48047', NULL, 10.5, 2180),
 (20699, 25.5, '2613 Camp Bella Plano TX 75074', NULL, 25.5, 4066),
-(54312, 1.98, '4005 Panther Way, Plano, TX 75074\r\n\r\n\r\n', 0.1, 1.88, 1234),
-(86868, 9.09, '8390 S. Sussex St. Glen Cove, NY 11542', NULL, 9.09, 6121);
+(84483, 2.97, '70 Oxford Street Garland, TX 75040', NULL, 2.97, 3230),
+(86868, 9.09, '8390 S. Sussex St. Glen Cove, NY 11542', NULL, 9.09, 6121),
+(89700, 7.48, '7791 Pulaski Street Haines City, FL 33844', NULL, 7.48, 8000);
 
 -- --------------------------------------------------------
 
@@ -131,15 +138,23 @@ CREATE TABLE `partof` (
 --
 
 INSERT INTO `partof` (`OrderID`, `itemID`, `quantity`) VALUES
-(20699, 1772, 1),
+(18375, 284, 1),
+(18375, 1029, 1),
+(18375, 1209, 1),
+(20379, 1928, 1),
+(20379, 2948, 1),
 (20699, 1928, 2),
 (20699, 2736, 1),
 (20699, 2938, 1),
 (20699, 2948, 1),
+(84483, 6382, 3),
 (86868, 1029, 1),
 (86868, 2736, 1),
 (86868, 2938, 1),
-(86868, 4917, 1);
+(86868, 4917, 1),
+(89700, 1029, 1),
+(89700, 1209, 2),
+(89700, 2736, 2);
 
 -- --------------------------------------------------------
 
@@ -173,7 +188,9 @@ INSERT INTO `warehouse` (`WarehouseID`, `Location`) VALUES
 (684, '23 Beaver Ridge Street\r\nHouston, TX 77033'),
 (822, '8275 Brickyard Ave.\r\nCleburne, TX 76031'),
 (839, '128 Walnut St.\r\nMissouri City, TX 77459'),
-(934, '12 Newport Street\r\nPasadena, TX 77506');
+(934, '12 Newport Street\r\nPasadena, TX 77506'),
+(937, '788 East Fifth Court\r\nSan Antonio, TX 78247'),
+(987, '315 Proctor Rd.\r\nGrapevine, TX 76051');
 
 --
 -- Indexes for dumped tables
@@ -190,26 +207,52 @@ ALTER TABLE `customer`
 -- Indexes for table `items`
 --
 ALTER TABLE `items`
-  ADD PRIMARY KEY (`itemID`);
+  ADD PRIMARY KEY (`itemID`),
+  ADD KEY `fk_items_warehouse` (`warehouseID`);
 
 --
 -- Indexes for table `order`
 --
 ALTER TABLE `order`
   ADD PRIMARY KEY (`OrderID`),
-  ADD UNIQUE KEY `OrderID` (`OrderID`);
+  ADD UNIQUE KEY `OrderID` (`OrderID`),
+  ADD KEY `fk_customer` (`CustomerID`);
 
 --
 -- Indexes for table `partof`
 --
 ALTER TABLE `partof`
-  ADD PRIMARY KEY (`OrderID`,`itemID`);
+  ADD PRIMARY KEY (`OrderID`,`itemID`),
+  ADD KEY `fk_items` (`itemID`);
 
 --
 -- Indexes for table `warehouse`
 --
 ALTER TABLE `warehouse`
   ADD PRIMARY KEY (`WarehouseID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `items`
+--
+ALTER TABLE `items`
+  ADD CONSTRAINT `fk_items_warehouse` FOREIGN KEY (`warehouseID`) REFERENCES `warehouse` (`WarehouseID`) ON DELETE NO ACTION;
+
+--
+-- Constraints for table `order`
+--
+ALTER TABLE `order`
+  ADD CONSTRAINT `fk_customer` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`customerID`) ON DELETE NO ACTION;
+
+--
+-- Constraints for table `partof`
+--
+ALTER TABLE `partof`
+  ADD CONSTRAINT `fk_items` FOREIGN KEY (`itemID`) REFERENCES `items` (`itemID`) ON DELETE NO ACTION,
+  ADD CONSTRAINT `fk_order` FOREIGN KEY (`OrderID`) REFERENCES `order` (`OrderID`) ON DELETE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
