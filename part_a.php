6389ff393ff64b8fce79bdec8d47c2c1232af96f
JonAbshiro <jonabshiro123@gmail.com>
2023-11-13 12:29:03 -0600

    Part A complete

--- part_a.php
+++ part_a.php
@@ -12,15 +12,44 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT imageName, itemID, itemName, price, quantity FROM items WHERE quantity > 0";
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM customer WHERE email = '$email' and password = '$password'";
+
 $result = $conn->query($sql);
+if ($result->num_rows > 0) {
+  // Fetch customer data
 
-$items = array();
-while($row = $result->fetch_assoc()) {
-  $items[] = $row;
+  $response = new stdClass();
+  while($customer = $result->fetch_assoc()) {
+    $response->customerID = $customer["customerID"];
+    $response->Name = $customer["Name"]; // Fetch the Name attribute
+  }
+
+
+  // Fetch customer orders
+  $orders_stmt = $conn->prepare("SELECT * FROM `order` WHERE CustomerID = ?");
+  $orders_stmt->bind_param("i", $response->customerID);
+
+  $orders_stmt->execute();
+
+  $orders_result = $orders_stmt->get_result();
+  $customer_orders = array();
+  if ($orders_result->num_rows > 0) {
+    while($order = $orders_result->fetch_assoc()) {
+      $customer_orders[] = $order;
+    }
+  } else {
+    $customer_orders = null;
+  }
+  $response->customerOrders = $customer_orders;
+  echo json_encode($response);
+  $orders_stmt->close();
+} else {
+  echo json_encode(array("error"=> "Invalid credentials"));
 }
-
-echo json_encode($items);
 
 $conn->close();
 ?>
