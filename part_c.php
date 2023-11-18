03fe56acfc96ad18f32c3353e00de5ee8e578f8e
vrao0409 <71802067+vrao0409@users.noreply.github.com>
2023-11-18 11:55:49 -0600

    initial test of part c

--- part_c.php
+++ part_c.php
@@ -12,15 +12,71 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT imageName, itemID, itemName, price, quantity FROM items WHERE quantity > 0";
-$result = $conn->query($sql);
 
-$items = array();
-while($row = $result->fetch_assoc()) {
-  $items[] = $row;
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql = "SELECT * 
+        FROM customer 
+        WHERE email = ? AND password = ?";
+
+if (!mysqli_stmt_prepare($stmt,$sql)) {
+    echo "failed";
+}
+else {
+    //bind parameters to placeholders
+    mysqli_stmt_bind_param($stmt, "ss", $email, $password );
+    //run parameters inside database
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo $customer['Name'];
+    }
 }
 
-echo json_encode($items);
+/*if ($stmt = $conn->prepare($sql)) {
+    $stmt->bind_param("ss", $email, $password);
+    $stmt->execute();
+
+    $stmt->bind_result($Name, $customerID);
+    while ($stmt->fetch()) {
+        printf ("%s %s\n",$Name, $customerID);
+    }
+}*/
+
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+  // Fetch customer data
+
+  $response = new stdClass();
+  while($customer = $result->fetch_assoc()) {
+    $response->customerID = $customer["customerID"];
+    $response->Name = $customer["Name"]; // Fetch the Name attribute
+  }
+
+
+  // Fetch customer orders
+  $orders_stmt = $conn->prepare("SELECT * FROM `order` WHERE CustomerID = ?");
+  $orders_stmt->bind_param("i", $response->customerID);
+
+  $orders_stmt->execute();
+
+  $orders_result = $orders_stmt->get_result();
+  $customer_orders = array();
+  if ($orders_result->num_rows > 0) {
+    while($order = $orders_result->fetch_assoc()) {
+      $customer_orders[] = $order;
+    }
+  } else {
+    $customer_orders = null;
+  }
+  $response->customerOrders = $customer_orders;
+  echo json_encode($response);
+  $orders_stmt->close();
+} else {
+  echo json_encode(array("error"=> "Invalid credentials"));
+}
 
 $conn->close();
 ?>
