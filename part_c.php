ae91ee296cbc742fe3c48a8d7269b6b409f97d80
sophiakobzar <31572998+sophiakobzar@users.noreply.github.com>
2023-11-18 12:34:25 -0600

    updated part c

--- part_c.php
+++ part_c.php
@@ -15,26 +15,44 @@ if ($conn->connect_error) {
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$sql = "SELECT * 
-        FROM customer 
-        WHERE email = ? AND password = ?";
+$sql = "SELECT * FROM customer WHERE email = ? and password = ?";
 
-$stmt = $conn->prepare($sql); // Define $stmt here
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $email, $password);
+$stmt->execute();
 
-if (!$stmt) {
-    echo "failed";
-}
-else {
-    //bind parameters to placeholders
-    $stmt->bind_param("ss", $email, $password );
-    //run parameters inside database
-    $stmt->execute();
-    $result = $stmt->get_result();
+$result = $stmt->get_result();
+if ($result->num_rows > 0) {
+  // Fetch customer data
 
-    while ($row = $result->fetch_assoc()) {
-        echo $row['Name'];
+  $response = new stdClass();
+  while($customer = $result->fetch_assoc()) {
+    $response->customerID = $customer["customerID"];
+    $response->Name = $customer["Name"]; // Fetch the Name attribute
+  }
+
+  // Fetch customer orders
+  $orders_stmt = $conn->prepare("SELECT * FROM `order` WHERE CustomerID = ?");
+  $orders_stmt->bind_param("i", $response->customerID);
+
+  $orders_stmt->execute();
+
+  $orders_result = $orders_stmt->get_result();
+  $customer_orders = array();
+  if ($orders_result->num_rows > 0) {
+    while($order = $orders_result->fetch_assoc()) {
+      $customer_orders[] = $order;
     }
+  } else {
+    $customer_orders = null;
+  }
+  $response->customerOrders = $customer_orders;
+  echo json_encode($response);
+  $orders_stmt->close();
+} else {
+  echo json_encode(array("error"=> "Invalid credentials"));
 }
 
+$stmt->close();
 $conn->close();
 ?>
